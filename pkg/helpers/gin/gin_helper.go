50788693900de0ec6d80c5f4377e553e350e72e3
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-08-27 11:31:17 +0600

    +

---
+++ pkg/helpers/gin/gin_helper.go
@@ -0,0 +1,17 @@
+package helpers
+
+import (
+	middleware "github.com/exgamer/go-rest-sdk/pkg/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRouter() *gin.Engine {
+	// Options
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.Use(middleware.ResponseHandler) // обработчик ошибок
+	router.Use(gin.Recovery())
+	router.Use(middleware.Recovery())
+
+	return router
+}
