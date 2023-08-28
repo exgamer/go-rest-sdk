d1990ef0e76d3e300712aae45007168fde10dcc3
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-08-28 13:20:08 +0600

    +

--- pkg/helpers/gin/gin_helper.go
+++ pkg/helpers/gin/gin_helper.go
@@ -3,15 +3,23 @@ package gin
 import (
 	"github.com/exgamer/go-rest-sdk/pkg/middleware"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitRouter() *gin.Engine {
 	// Options
 	router := gin.Default()
 	router.Use(gin.Logger())
-	router.Use(middleware.ResponseHandler) // обработчик ошибок
 	router.Use(gin.Recovery())
 	router.Use(middleware.Recovery())
+
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"code": "PAGE_NOT_FOUND", "message": "404 page not found"})
+	})
+
+	router.NoMethod(func(c *gin.Context) {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{"code": "METHOD_NOT_ALLOWED", "message": "405 method not allowed"})
+	})
 
 	return router
 }
