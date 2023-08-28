ab60e13aedaf6f1b152f2a214bb8b9bbde7babde
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-08-28 14:39:17 +0600

    +

--- pkg/helpers/validation/validation_helper.go
+++ pkg/helpers/validation/validation_helper.go
@@ -4,12 +4,12 @@ import (
 	"github.com/gookit/validate"
 )
 
-func ValidationErrorsAsMap(validationErrors validate.Errors) *map[string]any {
+func ValidationErrorsAsMap(validationErrors validate.Errors) map[string]any {
 	eMap := make(map[string]any)
 
 	for k, ve := range validationErrors {
 		eMap[k] = ve.String()
 	}
 
-	return &eMap
+	return eMap
 }
