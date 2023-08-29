b6dd666aa61a7e4e03d69e661979aabc80b59bd3
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-08-29 21:58:39 +0600

    +

--- pkg/config/structures/db.go
+++ pkg/config/structures/db.go
@@ -11,9 +11,9 @@ type DbConfig struct {
 	Db                       string `mapstructure:"DB_DATABASE" json:"db_database"`
 	Username                 string `mapstructure:"DB_USERNAME" json:"db_username"`
 	Password                 string `mapstructure:"DB_PASSWORD" json:"db_password"`
-	MaxPoolConnections       string `mapstructure:"DB_MAX_POOL_CONNECTIONS" json:"db_max_pool_connections"`
-	MaxIdlePoolConnections   string `mapstructure:"DB_MAX_IDLE_POOL_CONNECTIONS" json:"db_max_idle_pool_connections"`
-	ConnectionTimeoutSeconds string `mapstructure:"DB_CONNECTION_TIMEOUT_SECONDS" json:"db_connection_timeout_seconds"`
+	MaxPoolConnections       int    `mapstructure:"DB_MAX_POOL_CONNECTIONS" json:"db_max_pool_connections"`
+	MaxIdlePoolConnections   int    `mapstructure:"DB_MAX_IDLE_POOL_CONNECTIONS" json:"db_max_idle_pool_connections"`
+	ConnectionTimeoutSeconds int    `mapstructure:"DB_CONNECTION_TIMEOUT_SECONDS" json:"db_connection_timeout_seconds"`
 }
 
 func (a DbConfig) GetFieldsAsJsonTags() []string {
