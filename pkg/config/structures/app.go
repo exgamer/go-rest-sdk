e708979e4c457eef60e2f74667c77e1ebe5021f9
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-08-29 21:34:48 +0600

    +

--- pkg/config/structures/app.go
+++ pkg/config/structures/app.go
@@ -1,8 +1,25 @@
 package structures
 
+import (
+	"reflect"
+)
+
 type AppConfig struct {
 	Name          string `mapstructure:"APP_NAME" json:"app_name"`
 	HostName      string `mapstructure:"HOST_NAME" json:"host_name"`
 	Version       string `mapstructure:"APP_VERSION" json:"app_version"`
 	ServerAddress string `mapstructure:"SERVER_ADDRESS" json:"server_address"`
+}
+
+func (a AppConfig) GetFieldsAsJsonTags() []string {
+	result := make([]string, 0)
+
+	val := reflect.ValueOf(a)
+	t := val.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		result = append(result, t.Field(i).Tag.Get("json"))
+	}
+
+	return result
 }
--- pkg/config/structures/db.go
+++ pkg/config/structures/db.go
@@ -1,5 +1,7 @@
 package structures
 
+import "reflect"
+
 type DbConfig struct {
 	Host                     string `mapstructure:"DB_HOST" json:"db_host"`
 	Port                     string `mapstructure:"DB_PORT" json:"db_port"`
@@ -9,4 +11,17 @@ type DbConfig struct {
 	MaxPoolConnections       int    `mapstructure:"DB_MAX_POOL_CONNECTIONS" json:"db_max_pool_connections"`
 	MaxIdlePoolConnections   int    `mapstructure:"DB_MAX_IDLE_POOL_CONNECTIONS" json:"db_max_idle_connections"`
 	ConnectionTimeoutSeconds int64  `mapstructure:"DB_CONNECTION_TIMEOUT_SECONDS" json:"db_connection_timeout_seconds"`
+}
+
+func (a DbConfig) GetFieldsAsJsonTags() []string {
+	result := make([]string, 0)
+
+	val := reflect.ValueOf(a)
+	t := val.Type()
+
+	for i := 0; i < t.NumField(); i++ {
+		result = append(result, t.Field(i).Tag.Get("json"))
+	}
+
+	return result
 }
