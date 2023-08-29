aedeb24be035da7920a7eae6be27785cc96a9e62
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-08-29 19:12:51 +0600

    +

--- pkg/config/structures/app.go
+++ pkg/config/structures/app.go
@@ -1,8 +1,8 @@
 package structures
 
 type AppConfig struct {
-	Name          string `mapstructure:"APP_NAME"`
-	HostName      string `mapstructure:"HOST_NAME"`
-	Version       string `mapstructure:"APP_VERSION"`
-	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	Name          string `mapstructure:"APP_NAME" json:"app_name"`
+	HostName      string `mapstructure:"HOST_NAME" json:"host_name"`
+	Version       string `mapstructure:"APP_VERSION" json:"app_version"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS" json:"server_address"`
 }
--- pkg/config/structures/db.go
+++ pkg/config/structures/db.go
@@ -1,12 +1,12 @@
 package structures
 
 type DbConfig struct {
-	Host                     string `mapstructure:"DB_HOST"`
-	Port                     string `mapstructure:"DB_PORT"`
-	Db                       string `mapstructure:"DB_DATABASE"`
-	Username                 string `mapstructure:"DB_USERNAME"`
-	Password                 string `mapstructure:"DB_PASSWORD"`
-	MaxPoolConnections       int    `mapstructure:"DB_MAX_POOL_CONNECTIONS"`
-	MaxIdlePoolConnections   int    `mapstructure:"DB_MAX_IDLE_POOL_CONNECTIONS"`
-	ConnectionTimeoutSeconds int64  `mapstructure:"DB_CONNECTION_TIMEOUT_SECONDS"`
+	Host                     string `mapstructure:"DB_HOST" json:"db_host"`
+	Port                     string `mapstructure:"DB_PORT" json:"db_port"`
+	Db                       string `mapstructure:"DB_DATABASE" json:"db_database"`
+	Username                 string `mapstructure:"DB_USERNAME" json:"db_username"`
+	Password                 string `mapstructure:"DB_PASSWORD" json:"db_password"`
+	MaxPoolConnections       int    `mapstructure:"DB_MAX_POOL_CONNECTIONS" json:"db_max_pool_connections"`
+	MaxIdlePoolConnections   int    `mapstructure:"DB_MAX_IDLE_POOL_CONNECTIONS" json:"db_max_idle_connections"`
+	ConnectionTimeoutSeconds int64  `mapstructure:"DB_CONNECTION_TIMEOUT_SECONDS" json:"db_connection_timeout_seconds"`
 }
