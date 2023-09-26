2daec5ec991c4188181531e164e0b95b36bb76b0
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-09-26 10:47:27 +0600

    +

--- pkg/http/response.go
+++ pkg/http/response.go
@@ -7,6 +7,7 @@ import (
 
 func Error(c *gin.Context, exception *exception.AppException) {
 	c.Set("exception", exception)
+	c.Status(exception.Code)
 }
 
 func Response(c *gin.Context, status int, data any) {
