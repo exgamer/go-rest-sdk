76c36629dd3c0a07b0743e7f1bf27f64f3356690
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-10-19 15:24:40 +0600

    redis helper

--- pkg/helpers/db/redis/redis_helper.go
+++ pkg/helpers/db/redis/redis_helper.go
@@ -1,8 +1,27 @@
 package redis
 
 import (
+	"github.com/exgamer/go-rest-sdk/pkg/config/structures"
 	"github.com/gomodule/redigo/redis"
+	"time"
 )
+
+func GetPool(config structures.RedisConfig) *redis.Pool {
+	return &redis.Pool{
+		IdleTimeout:     time.Duration(config.IdleTimeout) * time.Second,
+		MaxConnLifetime: time.Duration(config.MaxConnLifetime) * time.Second,
+		MaxActive:       config.PoolSize,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", config.RedisHost)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return c, nil
+		},
+	}
+}
 
 func GetString(pool *redis.Pool, key string) (string, error) {
 	conn := pool.Get()
