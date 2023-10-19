b217f7fea47544538b6e930b9b4e37624e4ed09c
olzhaskulzhambekov <okulzhambekov@jmart.kz>
2023-10-19 10:08:35 +0600

    logs

--- pkg/modules/j/jHttpHelper/http_helper.go
+++ pkg/modules/j/jHttpHelper/http_helper.go
@@ -19,23 +19,24 @@ func DoGetHttpRequest(requestData *jStructures.RequestData, url string, headers 
 }
 
 func GetResponseBody(requestData *jStructures.RequestData, method string, url string, headers map[string]string, body io.Reader) ([]byte, error) {
+	messageBuilder := strings.Builder{}
+
 	start := time.Now()
 	response, err := http.DoHttpRequest(method, url, headers, body)
 	execTime := time.Since(start)
 
 	if err != nil {
 		logger.LogError(err)
+		messageBuilder.WriteString("Url: " + method + " " + url)
+		messageBuilder.WriteString(" Error:" + err.Error())
+
+		jLog.PrintInfoJLog(requestData.ServiceName, requestData.RequestMethod, requestData.RequestHost+requestData.RequestUrl, 0, requestData.RequestId, messageBuilder.String())
 
 		return nil, err
 	}
 
-	messageBuilder := strings.Builder{}
 	messageBuilder.WriteString("Url: " + response.Method + " " + response.Status + " " + response.Url)
 	messageBuilder.WriteString(" Exec time:" + execTime.String())
-
-	if err != nil {
-		messageBuilder.WriteString(" Error:" + err.Error())
-	}
 
 	if response.StatusCode != 200 {
 		messageBuilder.WriteString(" Response:" + string(response.Body))
